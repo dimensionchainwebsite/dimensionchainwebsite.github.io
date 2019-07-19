de62bb9264f5b648cc92f70b5cd9239aef6aa264
Boyang <i@boyang.im>
2019-07-19 15:00:46 +0800

    1
    1

---
+++ js/moving.js
@@ -0,0 +1,15 @@
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('permanentstorage'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'img/permanentstorage.json'
+})
+
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('authenticityverification'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'img/authenticityverification.json'
+})
